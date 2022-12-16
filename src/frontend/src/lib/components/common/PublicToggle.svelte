882e7a5ec210618fe1d612edb31b169cabf287d7
wannesds <wannesdieltiens@gmail.com>
2022-12-16 19:19:03 +0100

    added publicview eye svg's for checkbox

--- src/frontend/src/lib/components/common/PublicToggle.svelte
+++ src/frontend/src/lib/components/common/PublicToggle.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
+  import Eye from '$lib/assets/icons/eye.svg?component'
+  import EyeSlash from '$lib/assets/icons/eye-slash.svg?component'
 
-export let checked: boolean;
+  export let checked: boolean
+  // const onChange = () => {
+  //   checked = !checked
+  //   console.log('onchange checked:', checked)
+  // } 
+  $: { console.log('checked:', checked)}
 </script>
 
 
+<span class="toggle">
+  <label title="Public viewable" class="p-0.5">
+    <input
+      type="checkbox"
+      bind:checked={checked}
+     
+    />
+    {#if checked}
+      <Eye />
+    {:else if !checked }
+      <EyeSlash/>
+    {/if}
+  </label>
+</span>
 
-<input
-  type="checkbox"
-  bind:checked={checked}
-/>
+
+<style style lang="postcss">
+	.toggle input[type='checkbox'] {
+		display: none;
+	}
+	.toggle label {
+		@apply inline-block cursor-pointer relative transition-all ease-in-out duration-300 w-8 h-auto rounded-3xl bg-transparent;
+	}
+</style>
