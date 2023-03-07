c1ec5441f87139e65cbb22dbf2c48edc535f82c3
wannesds <wannesdieltiens@gmail.com>
2023-03-07 17:20:57 +0100

    changed YES/NO positions Qcard

--- src/frontend/src/lib/components/app/Qcard.svelte
+++ src/frontend/src/lib/components/app/Qcard.svelte
@@ -13,6 +13,7 @@
 	let answerPending: boolean | undefined = undefined;
 	let likePending: boolean | undefined = undefined;
 
+
 	const submitAnswer = async (bool: boolean) => {
 		answerPending = bool;
 		let answer: AnswerKind = { Bool: bool };
@@ -29,6 +30,7 @@
 	
 		answerPending = undefined;
 		likePending = undefined;
+		likeWeight = undefined;
 		getQs();
 	};
 
@@ -48,41 +50,16 @@
 	<div class="2xl:w-9/12 mx-auto rounded-md flex flex-col justify-center items-center">
 		<h2 class=" w-fit">{question.question}</h2>
 		<p class="text-slate-500 text-sm p-0">Created: {fromBigInt(question.created)}</p>
-		 <span>Hash: {question.hash}</span><!--
+		<p class="text-slate-500 text-sm p-0">By: {question.creater}</p>
+		<!--
   <span>Color: {question.color[0]}</span> -->
 		<!--   -->
 		<div
 			class="w-full flex flex-row justify-center items-center dark:text-slate-700 pt-3 gap-2 md:gap-4"
-		>
-	<!-- make these into own components -->
-			<button
-				on:click={() => likePending == true ? submitAnswer(true) : likePending = true}
-				disabled={skipPending || answerPending}
-				class="transition-all bg-green-400 hover:bg-green-500 h-16 w-4/12 rounded-xl flex justify-center items-center grow"
-			>
-				<h3>
-					{#if answerPending == true}
-						<Spinner />
-					{:else if likePending == true}
-						Submit
-					{:else}
-						YES
-					{/if}
-				</h3>
-			</button>
-			<button
-				on:click={skipQuestion}
-				disabled={skipPending || answerPending}
-				class="transition-all dark:bg-slate-600 h-16 w-16 md:w-36 rounded-xl dark:hover:bg-slate-800/70 bg-slate-200 hover:bg-slate-300 flex justify-center items-center"
-			>
-				<p class=" text-slate-400">
-					{#if !skipPending}
-						skip
-					{:else if skipPending}
-						<Spinner />
-					{/if}
-				</p>
-			</button>
+		>			
+		<!-- button sets likePending status after first click, second click submits the answer value and optionally the like value -->
+			
+
 			<button
 				on:click={() => likePending == false ? submitAnswer(false) : likePending = false}
 				disabled={skipPending || answerPending}
@@ -98,18 +75,57 @@
 					{/if}
 				</h3>
 			</button>
+			
+			<button
+				on:click={skipQuestion}
+				disabled={skipPending || answerPending}
+				class="transition-all dark:bg-slate-600 h-16 w-16 md:w-36 rounded-xl dark:hover:bg-slate-800/70 bg-slate-200 hover:bg-slate-300 flex justify-center items-center"
+			>
+				<p class=" text-slate-400">
+					{#if !skipPending}
+						skip
+					{:else if skipPending}
+						<Spinner />
+					{/if}
+				</p>
+			</button>
+
+			<button
+				on:click={() => likePending == true ? submitAnswer(true) : likePending = true}
+				disabled={skipPending || answerPending}
+				class="transition-all bg-green-400 hover:bg-green-500 h-16 w-4/12 rounded-xl flex justify-center items-center grow"
+			>
+				<h3>
+					{#if answerPending == true}
+						<Spinner />
+					{:else if likePending == true}
+						Submit
+					{:else}
+						YES
+					{/if}
+				</h3>
+			</button>
+
 		</div>
 
+
+
 		{#if likePending !== undefined} 
-		<p class="text-slate-500 text-sm p-0">Optionally: Rate how important this is for you.</p>
-			<div class="gap-2 flex ">
+		<!-- shows after first button click to optionally allow user to add a like value -->
+		<p class="text-slate-500 text-sm p-0 my-1">Optionally: Rate how important this is for you.</p>
+			<div class="flex gap-2">
 				{#each Array(10) as _, index}
-					<input type=radio bind:group={likeWeight} value={index}>
-					{index}
-					|
+				<label class="flex flex-col bg-slate-600 justify-content-center p-1 rounded-md">
+					<input type=radio bind:group={likeWeight} value={index + 1}>
+					<span class="mx-auto text-slate-500">{index + 1}</span>
+				</label>
+					
+					<!-- | -->
 				{/each}
 			</div>
 		{/if}
+
+
 
 	</div>
 </div>
