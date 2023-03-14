8e0dfed2f35c35305bcd5ad53358a771b74ab59b
wannesds <wannesdieltiens@gmail.com>
2023-03-14 17:11:01 +0100

    filterUI working, starting userCards

---
+++ src/frontend/src/lib/stores/tasks/getMatchedUsers.ts
@@ -0,0 +1,17 @@
+import { actor } from '$lib/stores';
+import type { User, MatchingFilter } from 'src/declarations/backend/backend.did';
+import { get, writable } from 'svelte/store';
+
+
+export const matchedUsers = writable<Array<User>>();
+
+export async function getMatchedUsers(filter : MatchingFilter) {
+	const localActor = get(actor);
+
+	return await localActor.findMatches(filter).then((res) => {
+		console.log('matched users: ', res.ok);
+		if (res.ok) {
+			matchedUsers.set(res.ok);
+		}
+	});
+}
