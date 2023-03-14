567eb0ab0bfe70134d4681831eb33e5a921e2ef6
wannesds <wannesdieltiens@gmail.com>
2023-03-14 12:13:07 +0100

    expand filters

--- src/frontend/src/routes/app/people/+page.svelte
+++ src/frontend/src/routes/app/people/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { user } from '$lib/stores/index';
+	import { makeExpiryTransform } from '@dfinity/agent';
+
+  let expandWindow : Boolean = false;
 
   const handleFindMatches = async () => {
 
@@ -11,8 +14,18 @@
   <div class="fancy-btn-border mx-auto mb-0">
     <button on:click={handleFindMatches} class="fancy-btn">Find a new connection</button>
   </div>
-  <span class="text-md font-semibold text-slate-600 mx-auto">Add filters +</span>
-
+  <button
+    class="w-full flex justify-between items-center"
+    on:click={() => (expandWindow = !expandWindow)}
+  >
+    <span class="text-md font-semibold text-slate-600 mx-auto">Add filters +</span>
+  </button>
+  
+  {#if expandWindow}
+    <div class="w-full md:w-2/3 mx-auto flex bg-slate-700">
+      test
+    </div>
+  {/if}
 	
 
 	<div class="dark:bg-slate-700 bg-slate-100 w-100% rounded-md flex flex-col p-2 md:p-8 gap-2">
