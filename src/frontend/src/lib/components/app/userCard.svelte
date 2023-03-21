71bac6a1d2fac21c59ad7bab88c11826fc4d0ed4
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2023-03-21 17:52:09 +0100

    age + gender showing on user card

--- src/frontend/src/lib/components/app/userCard.svelte
+++ src/frontend/src/lib/components/app/userCard.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { fromNullable , fromNullableGender} from '$lib/utilities';
 	import type { User, UserMatch } from 'src/declarations/backend/backend.did';
 	//export let match: [User, BigInt];
 	export let match: UserMatch;
+  let userBirth = fromNullable(match.birth); 
+	let ageMs = Number(new Date()) - Number(match.birth) / 1000000;
+	let ageY = Math.floor(ageMs / (1000 * 3600 * 24) / 365);
 
 	//TODO : change backend so it doesn't return a User obj,
 	//let it return the values that according user has made init public viewable
@@ -19,8 +23,7 @@ TODO : backend create friendlist and connection request implementation -->
 	<div class="grow flex flex-col">
 		<span class="text-sub text-2xl">{match.username}</span>
 		<span>{match.about}</span>
-		<span>{match.gender}</span>
-		<span>{match.birth}</span>
+		<span>{userBirth ? ageY : 'undefined'} , {fromNullableGender(match.gender)}</span>
 		<span>{match.score}</span>
 	</div>
 
