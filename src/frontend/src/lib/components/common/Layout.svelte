dabb9f9317ae1664b0e99b2f5419821304871256
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2023-04-25 18:09:20 +0200

    possible fix for nav icon bug on small phones

--- src/frontend/src/lib/components/common/Layout.svelte
+++ src/frontend/src/lib/components/common/Layout.svelte
@@ -2,7 +2,7 @@
 	import Logo from './Logo.svelte';
 </script>
 
-<div class="mx-auto min-h-screen flex flex-col scroll-smooth w-11/12">
+<div class="mx-auto min-h-screen flex flex-col scroll-smooth px-2 md:px-10">
 	<!-- header/nav -->
 
 	<div class="mx-auto bg-transparent flex justify-between h-12 w-full">
