02a853c9f888c8ed823b799ddf819c3a0e79c4e1
Martin Sigloch <f0i.git@projects.f0i.de>
2023-07-02 16:31:18 +0200

    Regenerate src/declartions

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -1,82 +1,119 @@
-import type { ActorMethod } from '@dfinity/agent';
 import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
 
-export type FriendStatus = { Approved: null } | { Waiting: null } | { Requested: null };
-export interface FriendlyUserMatch {
-	status: FriendStatus;
-	principal: Principal;
-	connect: [] | [string];
-	about: [] | [string];
-	username: string;
-	cohesion: bigint;
-	answered: Array<[Question, boolean]>;
-	picture: [] | [Uint8Array | number[]];
-	gender: [] | [Gender];
-	birth: [] | [bigint];
-	uncommon: Array<Question>;
+export interface Answer {
+  'weight' : bigint,
+  'question' : bigint,
+  'answer' : boolean,
 }
-export type Gender = { Male: null } | { Female: null } | { Other: null } | { Queer: null };
-export type Hash = number;
-export interface MatchingFilter {
-	cohesion: bigint;
-	ageRange: [bigint, bigint];
-	gender: [] | [Gender];
+export interface AnswerDiff {
+  'weight' : bigint,
+  'question' : bigint,
+  'sameAnswer' : boolean,
 }
+export type Error = { 'validationError' : null } |
+  { 'userNotFound' : null } |
+  { 'tooLong' : null } |
+  { 'insufficientFunds' : null } |
+  { 'notEnoughAnswers' : null } |
+  { 'tooShort' : null } |
+  { 'friendAlreadyConnected' : null } |
+  { 'nameNotAvailable' : null } |
+  { 'alreadyRegistered' : null } |
+  { 'friendRequestAlreadySend' : null } |
+  { 'notRegistered' : null } |
+  { 'invalidColor' : null };
+export type Gender = { 'Male' : null } |
+  { 'Female' : null } |
+  { 'Other' : null } |
+  { 'Queer' : null };
+export type IsPublic = boolean;
+export interface MatchingFilter { 'cohesion' : number, 'users' : UserFilter }
 export interface Question {
-	created: bigint;
-	creater: Principal;
-	question: string;
-	hash: Hash;
-	color: string;
-	points: bigint;
+  'id' : bigint,
+  'created' : Time__1,
+  'creator' : Principal,
+  'question' : string,
+  'color' : string,
+  'points' : bigint,
 }
 export interface Question__1 {
-	created: bigint;
-	creater: Principal;
-	question: string;
-	hash: Hash;
-	color: string;
-	points: bigint;
+  'id' : bigint,
+  'created' : Time__1,
+  'creator' : Principal,
+  'question' : string,
+  'color' : string,
+  'points' : bigint,
 }
-export type Result = { ok: null } | { err: string };
-export type Result_1 = { ok: User } | { err: string };
-export type Result_2 = { ok: Array<FriendlyUserMatch> } | { err: string };
-export type Result_3 = { ok: Array<Question__1> } | { err: string };
-export type Result_4 = { ok: UserMatch } | { err: string };
+export type Result = { 'ok' : User } |
+  { 'err' : Error };
+export type Result_1 = { 'ok' : Skip } |
+  { 'err' : Error };
+export type Result_2 = { 'ok' : Answer } |
+  { 'err' : Error };
+export type Result_3 = { 'ok' : null } |
+  { 'err' : Error };
+export type Result_4 = { 'ok' : Array<UserMatch> } |
+  { 'err' : Error };
+export type Result_5 = { 'ok' : UserMatch } |
+  { 'err' : Error };
+export type Result_6 = { 'ok' : Question__1 } |
+  { 'err' : Error };
+export interface Skip {
+  'question' : bigint,
+  'reason' : { 'flag' : null } |
+    { 'skip' : null },
+}
+export interface Social { 'network' : SocialNetwork, 'handle' : string }
+export type SocialNetwork = { 'mastodon' : null } |
+  { 'twitter' : null } |
+  { 'email' : null } |
+  { 'distrikt' : null } |
+  { 'phone' : null } |
+  { 'dscvr' : null };
+export type Time = bigint;
+export type Time__1 = bigint;
 export interface User {
-	created: bigint;
-	connect: [[] | [string], boolean];
-	about: [[] | [string], boolean];
-	username: string;
-	picture: [[] | [Uint8Array | number[]], boolean];
-	gender: [[] | [Gender], boolean];
-	birth: [[] | [bigint], boolean];
-	points: bigint;
+  'created' : Time,
+  'about' : [[] | [string], IsPublic],
+  'username' : string,
+  'socials' : Array<[Social, IsPublic]>,
+  'picture' : [[] | [Uint8Array | number[]], IsPublic],
+  'gender' : [[] | [Gender], IsPublic],
+  'birth' : [[] | [Time], IsPublic],
+  'points' : bigint,
+}
+export interface UserFilter {
+  'maxBirth' : Time,
+  'gender' : [] | [Gender],
+  'minBirth' : Time,
+}
+export interface UserInfo {
+  'about' : [] | [string],
+  'username' : string,
+  'socials' : Array<Social>,
+  'picture' : [] | [Uint8Array | number[]],
+  'gender' : [] | [Gender],
+  'birth' : [] | [Time],
 }
 export interface UserMatch {
-	principal: Principal;
-	connect: [] | [string];
-	about: [] | [string];
-	username: string;
-	cohesion: bigint;
-	answered: Array<[Question, boolean]>;
-	picture: [] | [Uint8Array | number[]];
-	gender: [] | [Gender];
-	birth: [] | [bigint];
-	uncommon: Array<Question>;
+  'cohesion' : number,
+  'answered' : Array<[Question, AnswerDiff]>,
+  'user' : UserInfo,
+  'uncommon' : Array<Question>,
 }
 export interface _SERVICE {
-	answerFriendRequest: ActorMethod<[Principal, boolean], Result>;
-	createQuestion: ActorMethod<[string, string], Result>;
-	createUser: ActorMethod<[string], Result>;
-	findMatch: ActorMethod<[MatchingFilter], Result_4>;
-	getAnsweredQuestions: ActorMethod<[], Result_3>;
-	getAskableQuestions: ActorMethod<[bigint], Result_3>;
-	getFriends: ActorMethod<[], Result_2>;
-	getUser: ActorMethod<[], Result_1>;
-	sendFriendRequest: ActorMethod<[Principal], Result>;
-	submitAnswer: ActorMethod<[Hash, boolean, bigint], Result>;
-	submitSkip: ActorMethod<[Hash], Result>;
-	updateProfile: ActorMethod<[User], Result>;
-	whoami: ActorMethod<[], Principal>;
+  'answerFriendRequest' : ActorMethod<[string, boolean], Result_3>,
+  'createQuestion' : ActorMethod<[string, string], Result_6>,
+  'createUser' : ActorMethod<[string], Result>,
+  'findMatch' : ActorMethod<[MatchingFilter], Result_5>,
+  'getAnsweredQuestions' : ActorMethod<[bigint], Array<[Question__1, Answer]>>,
+  'getAskableQuestions' : ActorMethod<[bigint], Array<Question__1>>,
+  'getFriends' : ActorMethod<[], Result_4>,
+  'getUser' : ActorMethod<[], Result>,
+  'sendFriendRequest' : ActorMethod<[string], Result_3>,
+  'submitAnswer' : ActorMethod<[bigint, boolean, bigint], Result_2>,
+  'submitSkip' : ActorMethod<[bigint], Result_1>,
+  'updateProfile' : ActorMethod<[User], Result>,
+  'whoami' : ActorMethod<[], Principal>,
 }
--- src/declarations/backend/backend.did.js
+++ src/declarations/backend/backend.did.js
@@ -1,99 +1,128 @@
 export const idlFactory = ({ IDL }) => {
-	const Result = IDL.Variant({ ok: IDL.Null, err: IDL.Text });
-	const Gender = IDL.Variant({
-		Male: IDL.Null,
-		Female: IDL.Null,
-		Other: IDL.Null,
-		Queer: IDL.Null
-	});
-	const MatchingFilter = IDL.Record({
-		cohesion: IDL.Int,
-		ageRange: IDL.Tuple(IDL.Nat, IDL.Nat),
-		gender: IDL.Opt(Gender)
-	});
-	const Hash = IDL.Nat32;
-	const Question = IDL.Record({
-		created: IDL.Int,
-		creater: IDL.Principal,
-		question: IDL.Text,
-		hash: Hash,
-		color: IDL.Text,
-		points: IDL.Int
-	});
-	const UserMatch = IDL.Record({
-		principal: IDL.Principal,
-		connect: IDL.Opt(IDL.Text),
-		about: IDL.Opt(IDL.Text),
-		username: IDL.Text,
-		cohesion: IDL.Int,
-		answered: IDL.Vec(IDL.Tuple(Question, IDL.Bool)),
-		picture: IDL.Opt(IDL.Vec(IDL.Nat8)),
-		gender: IDL.Opt(Gender),
-		birth: IDL.Opt(IDL.Int),
-		uncommon: IDL.Vec(Question)
-	});
-	const Result_4 = IDL.Variant({ ok: UserMatch, err: IDL.Text });
-	const Question__1 = IDL.Record({
-		created: IDL.Int,
-		creater: IDL.Principal,
-		question: IDL.Text,
-		hash: Hash,
-		color: IDL.Text,
-		points: IDL.Int
-	});
-	const Result_3 = IDL.Variant({
-		ok: IDL.Vec(Question__1),
-		err: IDL.Text
-	});
-	const FriendStatus = IDL.Variant({
-		Approved: IDL.Null,
-		Waiting: IDL.Null,
-		Requested: IDL.Null
-	});
-	const FriendlyUserMatch = IDL.Record({
-		status: FriendStatus,
-		principal: IDL.Principal,
-		connect: IDL.Opt(IDL.Text),
-		about: IDL.Opt(IDL.Text),
-		username: IDL.Text,
-		cohesion: IDL.Int,
-		answered: IDL.Vec(IDL.Tuple(Question, IDL.Bool)),
-		picture: IDL.Opt(IDL.Vec(IDL.Nat8)),
-		gender: IDL.Opt(Gender),
-		birth: IDL.Opt(IDL.Int),
-		uncommon: IDL.Vec(Question)
-	});
-	const Result_2 = IDL.Variant({
-		ok: IDL.Vec(FriendlyUserMatch),
-		err: IDL.Text
-	});
-	const User = IDL.Record({
-		created: IDL.Int,
-		connect: IDL.Tuple(IDL.Opt(IDL.Text), IDL.Bool),
-		about: IDL.Tuple(IDL.Opt(IDL.Text), IDL.Bool),
-		username: IDL.Text,
-		picture: IDL.Tuple(IDL.Opt(IDL.Vec(IDL.Nat8)), IDL.Bool),
-		gender: IDL.Tuple(IDL.Opt(Gender), IDL.Bool),
-		birth: IDL.Tuple(IDL.Opt(IDL.Int), IDL.Bool),
-		points: IDL.Nat
-	});
-	const Result_1 = IDL.Variant({ ok: User, err: IDL.Text });
-	return IDL.Service({
-		answerFriendRequest: IDL.Func([IDL.Principal, IDL.Bool], [Result], []),
-		createQuestion: IDL.Func([IDL.Text, IDL.Text], [Result], []),
-		createUser: IDL.Func([IDL.Text], [Result], []),
-		findMatch: IDL.Func([MatchingFilter], [Result_4], []),
-		getAnsweredQuestions: IDL.Func([], [Result_3], ['query']),
-		getAskableQuestions: IDL.Func([IDL.Nat], [Result_3], ['query']),
-		getFriends: IDL.Func([], [Result_2], ['query']),
-		getUser: IDL.Func([], [Result_1], ['query']),
-		sendFriendRequest: IDL.Func([IDL.Principal], [Result], []),
-		submitAnswer: IDL.Func([Hash, IDL.Bool, IDL.Int], [Result], []),
-		submitSkip: IDL.Func([Hash], [Result], []),
-		updateProfile: IDL.Func([User], [Result], []),
-		whoami: IDL.Func([], [IDL.Principal], ['query'])
-	});
+  const Error = IDL.Variant({
+    'validationError' : IDL.Null,
+    'userNotFound' : IDL.Null,
+    'tooLong' : IDL.Null,
+    'insufficientFunds' : IDL.Null,
+    'notEnoughAnswers' : IDL.Null,
+    'tooShort' : IDL.Null,
+    'friendAlreadyConnected' : IDL.Null,
+    'nameNotAvailable' : IDL.Null,
+    'alreadyRegistered' : IDL.Null,
+    'friendRequestAlreadySend' : IDL.Null,
+    'notRegistered' : IDL.Null,
+    'invalidColor' : IDL.Null,
+  });
+  const Result_3 = IDL.Variant({ 'ok' : IDL.Null, 'err' : Error });
+  const Time__1 = IDL.Int;
+  const Question__1 = IDL.Record({
+    'id' : IDL.Nat,
+    'created' : Time__1,
+    'creator' : IDL.Principal,
+    'question' : IDL.Text,
+    'color' : IDL.Text,
+    'points' : IDL.Int,
+  });
+  const Result_6 = IDL.Variant({ 'ok' : Question__1, 'err' : Error });
+  const Time = IDL.Int;
+  const IsPublic = IDL.Bool;
+  const SocialNetwork = IDL.Variant({
+    'mastodon' : IDL.Null,
+    'twitter' : IDL.Null,
+    'email' : IDL.Null,
+    'distrikt' : IDL.Null,
+    'phone' : IDL.Null,
+    'dscvr' : IDL.Null,
+  });
+  const Social = IDL.Record({ 'network' : SocialNetwork, 'handle' : IDL.Text });
+  const Gender = IDL.Variant({
+    'Male' : IDL.Null,
+    'Female' : IDL.Null,
+    'Other' : IDL.Null,
+    'Queer' : IDL.Null,
+  });
+  const User = IDL.Record({
+    'created' : Time,
+    'about' : IDL.Tuple(IDL.Opt(IDL.Text), IsPublic),
+    'username' : IDL.Text,
+    'socials' : IDL.Vec(IDL.Tuple(Social, IsPublic)),
+    'picture' : IDL.Tuple(IDL.Opt(IDL.Vec(IDL.Nat8)), IsPublic),
+    'gender' : IDL.Tuple(IDL.Opt(Gender), IsPublic),
+    'birth' : IDL.Tuple(IDL.Opt(Time), IsPublic),
+    'points' : IDL.Nat,
+  });
+  const Result = IDL.Variant({ 'ok' : User, 'err' : Error });
+  const UserFilter = IDL.Record({
+    'maxBirth' : Time,
+    'gender' : IDL.Opt(Gender),
+    'minBirth' : Time,
+  });
+  const MatchingFilter = IDL.Record({
+    'cohesion' : IDL.Nat8,
+    'users' : UserFilter,
+  });
+  const Question = IDL.Record({
+    'id' : IDL.Nat,
+    'created' : Time__1,
+    'creator' : IDL.Principal,
+    'question' : IDL.Text,
+    'color' : IDL.Text,
+    'points' : IDL.Int,
+  });
+  const AnswerDiff = IDL.Record({
+    'weight' : IDL.Nat,
+    'question' : IDL.Nat,
+    'sameAnswer' : IDL.Bool,
+  });
+  const UserInfo = IDL.Record({
+    'about' : IDL.Opt(IDL.Text),
+    'username' : IDL.Text,
+    'socials' : IDL.Vec(Social),
+    'picture' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'gender' : IDL.Opt(Gender),
+    'birth' : IDL.Opt(Time),
+  });
+  const UserMatch = IDL.Record({
+    'cohesion' : IDL.Nat8,
+    'answered' : IDL.Vec(IDL.Tuple(Question, AnswerDiff)),
+    'user' : UserInfo,
+    'uncommon' : IDL.Vec(Question),
+  });
+  const Result_5 = IDL.Variant({ 'ok' : UserMatch, 'err' : Error });
+  const Answer = IDL.Record({
+    'weight' : IDL.Nat,
+    'question' : IDL.Nat,
+    'answer' : IDL.Bool,
+  });
+  const Result_4 = IDL.Variant({ 'ok' : IDL.Vec(UserMatch), 'err' : Error });
+  const Result_2 = IDL.Variant({ 'ok' : Answer, 'err' : Error });
+  const Skip = IDL.Record({
+    'question' : IDL.Nat,
+    'reason' : IDL.Variant({ 'flag' : IDL.Null, 'skip' : IDL.Null }),
+  });
+  const Result_1 = IDL.Variant({ 'ok' : Skip, 'err' : Error });
+  return IDL.Service({
+    'answerFriendRequest' : IDL.Func([IDL.Text, IDL.Bool], [Result_3], []),
+    'createQuestion' : IDL.Func([IDL.Text, IDL.Text], [Result_6], []),
+    'createUser' : IDL.Func([IDL.Text], [Result], []),
+    'findMatch' : IDL.Func([MatchingFilter], [Result_5], []),
+    'getAnsweredQuestions' : IDL.Func(
+        [IDL.Nat],
+        [IDL.Vec(IDL.Tuple(Question__1, Answer))],
+        ['query'],
+      ),
+    'getAskableQuestions' : IDL.Func(
+        [IDL.Nat],
+        [IDL.Vec(Question__1)],
+        ['query'],
+      ),
+    'getFriends' : IDL.Func([], [Result_4], ['query']),
+    'getUser' : IDL.Func([], [Result], ['query']),
+    'sendFriendRequest' : IDL.Func([IDL.Text], [Result_3], []),
+    'submitAnswer' : IDL.Func([IDL.Nat, IDL.Bool, IDL.Nat], [Result_2], []),
+    'submitSkip' : IDL.Func([IDL.Nat], [Result_1], []),
+    'updateProfile' : IDL.Func([User], [Result], []),
+    'whoami' : IDL.Func([], [IDL.Principal], ['query']),
+  });
 };
-export const init = ({ IDL }) => {
-	return [];
-};
+export const init = ({ IDL }) => { return []; };
