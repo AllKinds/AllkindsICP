33f05571eda08f42771e5396c13f33dac3bd8515
dk <dk@dk-macbook.Dlink>
2023-07-10 13:52:48 +0200

    Instractions to registration added

--- src/frontend/src/routes/register/finished/+page.svelte
+++ src/frontend/src/routes/register/finished/+page.svelte
@@ -9,7 +9,10 @@
 
 <div class="flex flex-col justify-center items-center">
 	<h2>Well done!</h2>
-	<span>Profile finished</span>
+	<span><li>Ask Yes/No questions and answer other people's questions.
+		<li>Use +/- to indicate the importance of the question. Earn diamonds by creating questions.
+		<li>Search for people that resonate the most (or the least).
+		<li>Reach out to each other using emails.</span>
 	<div class="w-fit h-fit mx-auto">
 		<div class="fancy-btn-border">
 			<button on:click={submit} class="fancy-btn">Continue</button>
