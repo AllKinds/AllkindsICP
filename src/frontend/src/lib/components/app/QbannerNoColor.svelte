af4b403cb7b13f08ef8b852f05ad9943ec09a788
dk <dk@dk-macbook.local>
2023-07-13 16:40:55 +0200

    spelling changes

--- src/frontend/src/lib/components/app/QbannerNoColor.svelte
+++ src/frontend/src/lib/components/app/QbannerNoColor.svelte
@@ -16,6 +16,6 @@
 </script>
 
 <div class="h-18 w-full flex justify-between rounded-lg px-2 py-1 bg-sub text-main " id="banner">
-	{q.question}
+	{q.question}..
 	<!-- <slot name="weight" /> -->
 </div>
