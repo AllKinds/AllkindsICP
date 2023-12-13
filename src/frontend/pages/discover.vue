125da012ea0f3e8befaa70892eb2ee07bf010d99
Martin Sigloch <f0i.git@projects.f0i.de>
2023-12-13 23:11:15 +0100

    Add some tailwind to discover.vue

--- src/frontend/pages/discover.vue
+++ src/frontend/pages/discover.vue
@@ -22,15 +22,16 @@ if (inBrowser()) {
 
     </AllkindsTitle>
 
-    <NetworkDataContainer :networkdata="app.getMatches()">
+    <NetworkDataContainer :networkdata="app.getMatches()" class="w-96">
         <div v-if="app.getMatches().data?.length === 0">
             No matches found.
             <br>
             You need at least 5 questions in common to see each others coherence score.
         </div>
 
-        <div v-for="match in app.getMatches().data">
-            {{ match.user.username }}: {{ match.cohesion }} ({{ match.answered.length }})
+        <div v-for="match in app.getMatches().data" class="text-xl flex flex-row w-full p-3">
+            <div class="flex-grow">{{ match.user.displayName }}</div>
+            <div>{{ match.cohesion }} ({{ match.answered.length }})</div>
         </div>
     </NetworkDataContainer>
 </template>
