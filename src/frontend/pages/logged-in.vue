d6a317090f73edd491704a96a436441b99577f02
Martin Sigloch <f0i.git@projects.f0i.de>
2024-02-14 15:20:22 +0100

    Change login flow

---
+++ src/frontend/pages/logged-in.vue
@@ -0,0 +1,29 @@
+<!--
+Page to check user registration state
+-->
+
+<script lang="ts" setup>
+
+definePageMeta({
+    title: "Allkinds",
+    layout: 'default'
+});
+
+const app = useAppState();
+
+if (inBrowser()) {
+    // loadUser will redirect to /register or /login if user doesn't exist
+    app.loadUser(0).then(
+        (u) => { navigateTo("/questions") }
+    );
+}
+
+</script>
+
+<template>
+    <div class="grow flex flex-col items-center">
+        <div class="w-full flex-grow h-10"></div>
+        <Icon name="line-md:loading-alt-loop" size="5em" class="absolute mt-8" />
+        <div class="w-full flex-grow"></div>
+    </div>
+</template>
--- src/frontend/pages/questions.vue
+++ src/frontend/pages/questions.vue
@@ -2,18 +2,18 @@
 import NumberAnimation from 'vue-number-animation';
 
 definePageMeta({
-  title: "Allkinds",
-  layout: 'default'
+    title: "Allkinds",
+    layout: 'default'
 });
 
 const app = useAppState();
 const user = () => app.getUser().data?.user;
 
 if (inBrowser()) {
-  app.getTeam();
-  app.loadOpenQuestions(0);
-  app.loadUser();
-  app.loadTeams();
+    app.getTeam();
+    app.loadOpenQuestions(0);
+    app.loadUser();
+    app.loadTeams();
 }
 const balance = ref(null);
 setTimeout(() => balance.value?.restart(), 300)
@@ -21,44 +21,44 @@ setTimeout(() => balance.value?.restart(), 300)
 
 
 <template>
-  <div class="w-full flex-grow flex flex-col">
-    <AllkindsTitle link-to="/team-stats" :logo-url="toDataUrl(app.getTeam()?.info.logo || [])">
-      {{ app.checkTeam() }}
-      <NuxtLink to="/my-profile" class="m-auto">
-        {{ user()?.displayName }},
-        <NumberAnimation :to="user()?.stats.points" :from=0 ref="balance" duration=0.3 autoplay easing="linear"
-          :format="Math.round" />
-        <Icon name="gg:shape-hexagon" class="mb-2" />
-      </NuxtLink>
+    <div class="w-full flex-grow flex flex-col">
+        <AllkindsTitle link-to="/team-stats" :logo-url="toDataUrl(app.getTeam()?.info.logo || [])">
+            {{ app.checkTeam() }}
+            <NuxtLink to="/my-profile" class="m-auto">
+                {{ user()?.displayName }},
+                <NumberAnimation :to="user()?.stats.points" :from=0 ref="balance" duration=0.3 autoplay easing="linear"
+                    :format="Math.round" />
+                <Icon name="gg:shape-hexagon" class="mb-2" />
+            </NuxtLink>
 
-      <template #action>
-        <IconLink to="/discover" />
-      </template>
-    </AllkindsTitle>
+            <template #action>
+                <IconLink to="/discover" />
+            </template>
+        </AllkindsTitle>
 
-    <NuxtLink to="/ask-question" class="w-full rounded bg-slate-100 text-gray-500 text-xl p-5 cursor-text">
-      Ask your yes/no question
-    </NuxtLink>
+        <Btn to="/ask-question" class="w-96 m-5">
+            Ask your yes/no question
+        </Btn>
 
-    <NetworkDataContainer :networkdata="app.getOpenQuestions()" class="grow mt-4 w-full">
-      <Question v-for="( q, _ ) in  app.getOpenQuestions().data " :question="q" :link="true" />
+        <NetworkDataContainer :networkdata="app.getOpenQuestions()" class="grow mt-4 w-full">
+            <Question v-for="( q, _ ) in  app.getOpenQuestions().data " :question="q" :link="true" />
 
-      <div v-if="app.getOpenQuestions().data?.length === 0">
-        There are no more questions at the moment.
+            <div v-if="app.getOpenQuestions().data?.length === 0">
+                There are no more questions at the moment.
 
-        <p>
-          Ask one above or check back later.
-        </p>
+                <p>
+                    Ask one above or check back later.
+                </p>
 
-        <div class="grow" />
-        <div class="w-full text-center my-10">
-          <Btn @click="app.loadOpenQuestions(0, 'Questions loaded')">
-            Refresh &nbsp;
-            <Icon name="charm:refresh" />
-          </Btn>
-        </div>
-        <div class="grow" />
-      </div>
-    </NetworkDataContainer>
-  </div>
+                <div class="grow" />
+                <div class="w-full text-center my-10">
+                    <Btn @click="app.loadOpenQuestions(0, 'Questions loaded')">
+                        Refresh &nbsp;
+                        <Icon name="charm:refresh" />
+                    </Btn>
+                </div>
+                <div class="grow" />
+            </div>
+        </NetworkDataContainer>
+    </div>
 </template>
