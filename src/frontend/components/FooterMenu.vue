af7aa35fc570bf8b861f3a77f91c7b5d468184be
Martin Sigloch <f0i.git@projects.f0i.de>
2024-02-19 19:56:52 +0100

    Fix bg color for footer menu

--- src/frontend/components/FooterMenu.vue
+++ src/frontend/components/FooterMenu.vue
@@ -4,19 +4,23 @@
 </script>
 <template>
     <div class="w-full h-20 flex flex-row justify-between text-sm">
-        <NuxtLink to="/questions" class="p-[7px] w-auto m-0 flex flex-col items-center hover:invert bg-black rounded-lg">
+        <NuxtLink to="/questions"
+            class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/questions').icon" size="2rem" class="mt-2" />
             Questions
         </NuxtLink>
-        <NuxtLink to="/discover" class="p-[7px] w-auto m-0 flex flex-col items-center hover:invert bg-black rounded-lg">
+        <NuxtLink to="/discover"
+            class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/discover').icon" size="2rem" class="mt-2" />
             Discover
         </NuxtLink>
-        <NuxtLink to="/contacts" class="p-[7px] w-auto m-0 flex flex-col items-center hover:invert bg-black rounded-lg">
+        <NuxtLink to="/contacts"
+            class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/contacts').icon" size="2rem" class="mt-2" />
             My contacts
         </NuxtLink>
-        <NuxtLink to="/my-profile" class="p-[7px] w-auto m-0 flex flex-col items-center hover:invert bg-black rounded-lg">
+        <NuxtLink to="/my-profile"
+            class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/my-profile').icon" size="2rem" class="mt-2" />
             My profile
         </NuxtLink>
