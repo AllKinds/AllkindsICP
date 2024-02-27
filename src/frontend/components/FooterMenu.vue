677cd5d21020b92a7dd1a7f70ae17bbb0c37c89d
Martin Sigloch <f0i.git@projects.f0i.de>
2024-02-27 10:31:38 +0100

    Highlight active footer menu item

--- src/frontend/components/FooterMenu.vue
+++ src/frontend/components/FooterMenu.vue
@@ -4,22 +4,22 @@
 </script>
 <template>
     <div class="w-full h-20 grid grid-cols-4 gap-1 text-xs">
-        <NuxtLink to="/questions"
+        <NuxtLink to="/questions" :class="{ 'bg-slate-700': $route.path === '/questions' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/questions').icon" size="2rem" class="mt-2" />
             Questions
         </NuxtLink>
-        <NuxtLink to="/discover"
+        <NuxtLink to="/discover" :class="{ 'bg-slate-700': $route.path === '/discover' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/discover').icon" size="2rem" class="mt-2" />
             Discover
         </NuxtLink>
-        <NuxtLink to="/contacts"
+        <NuxtLink to="/contacts" :class="{ 'bg-slate-700': $route.path === '/contacts' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/contacts').icon" size="2rem" class="mt-2" />
             My&nbsp;contacts
         </NuxtLink>
-        <NuxtLink to="/my-profile"
+        <NuxtLink to="/my-profile" :class="{ 'bg-slate-700': $route.path === '/my-profile' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg">
             <Icon :name="getIcon('/my-profile').icon" size="2rem" class="mt-2" />
             My&nbsp;profile
