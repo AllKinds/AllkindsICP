c11e87354d84d4d9476d725e1c0f5c9f05e6d6b2
Martin Sigloch <f0i.git@projects.f0i.de>
2024-03-22 11:59:43 +0000

    Make notifications match figma design

--- src/frontend/components/FooterMenu.vue
+++ src/frontend/components/FooterMenu.vue
@@ -27,7 +27,7 @@ if (inBrowser()) {
     <div class="w-full h-20 grid grid-cols-4 gap-1 text-xs">
         <NuxtLink to="/questions" :class="{ 'menu-active': $route.path === '/questions' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg relative">
-            <span v-if="notification('newQuestions', app.team)" class="absolute right-2 badge badge-error px-1">{{
+            <span v-if="notification('newQuestions', app.team)" class="absolute right-2 text-sm text-red-600 px-5">{{
                 notification('newQuestions', app.team) }}</span>
             <Icon :name="getIcon('/questions').icon" size="2rem" class="mt-2" />
             Questions
@@ -39,7 +39,7 @@ if (inBrowser()) {
         </NuxtLink>
         <NuxtLink to="/contacts" :class="{ 'menu-active': $route.path === '/contacts' }"
             class="p-[7px] w-auto m-0 flex flex-col items-center hover:text-black hover:bg-white rounded-lg relative">
-            <span v-if="notification('friendRequests', app.team)" class="absolute right-2 badge badge-error px-1">{{
+            <span v-if="notification('friendRequests', app.team)" class="absolute right-2 text-sm text-red-600 px-5">{{
                 notification('friendRequests', app.team) }}</span>
             <Icon :name="getIcon('/contacts').icon" size="2rem" class="mt-2" />
             My&nbsp;contacts
